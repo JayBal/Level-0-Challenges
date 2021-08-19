fd1b824f25fdfb028802c8085902ccbcd231517d
Jacky Baloyi <87927960+JayBal@users.noreply.github.com>
2021-08-19 19:31:00 +0000

    Update task0-10.js

--- task0-10.js
+++ task0-10.js
@@ -1,19 +1,19 @@
 function commonCharacterFinder(inputOne, inputTwo) {
-    var charFoundHolder = " ";
-    if (inputOne.length > inputTwo.length) {
-        for (let i = 0; i < inputOne.length; i++) {
-            if (inputOne.includes(inputTwo[i])) {
-                charFoundHolder += inputTwo[i] + " ";
-            }
-        }
-    } else {
-        for (let i = 0; i < inputTwo.length; i++) {
-            if (inputOne.includes(inputTwo[i])) {
-                charFoundHolder += inputTwo[i] + " ";
-            }
-        }
+  var charFoundHolder = " ";
+  if (inputOne.length > inputTwo.length) {
+    for (let i = 0; i < inputOne.length; i++) {
+      if (inputOne.includes(inputTwo[i])) {
+        charFoundHolder += inputTwo[i] + ",";
+      }
     }
-    console.log("Common letters:" + charFoundHolder);
+  } else {
+    for (let i = 0; i < inputTwo.length; i++) {
+      if (inputOne.includes(inputTwo[i])) {
+        charFoundHolder += inputTwo[i] + " ";
+      }
+    }
+  }
+  console.log("Common letters:" + charFoundHolder);
 }
 
 commonCharacterFinder("computers", "house");
