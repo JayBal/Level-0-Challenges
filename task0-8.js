e844ac56388cc0b574253b511a42791f6200e6c9
Jacky Baloyi <87927960+JayBal@users.noreply.github.com>
2021-08-19 19:27:36 +0000

    Update task0-8.js

--- task0-8.js
+++ task0-8.js
@@ -1,32 +1,35 @@
-var initialHour;
+function realNumberToTime(number) {
+  var initialHour;
 
-var number = 71;
-initialHour = number / 60;
+  //var number = 71;
+  initialHour = number / 60;
 
-hourManipulator = parseFloat(initialHour).toFixed(0); // I came up with this just so I can eleminate decimals
-//hourManipulator = initialHour; : This uncommented will produce 1.18 with 71 as the given number.
-minuteManipulator = number % 60;
-final = parseFloat(hourManipulator) + parseFloat(minuteManipulator);
+  hourManipulator = parseFloat(initialHour).toFixed(0); // I came up with this just so I can eleminate decimals
+  //hourManipulator = initialHour; : This uncommented will produce 1.18 with 71 as the given number.
+  minuteManipulator = number % 60;
+  final = parseFloat(hourManipulator) + parseFloat(minuteManipulator);
 
-var pluralDeterminerHours = " hour";
-var pluralDeterminerMinutes = " minute ";
+  var pluralDeterminerHours = " hour";
+  var pluralDeterminerMinutes = " minute ";
 
-if (hourManipulator > 1) {
-  pluralDeterminerHours = " hours ";
-} else if (pluralDeterminerHours == 0) {
-  pluralDeterminerHours = " hours ";
+  if (hourManipulator > 1) {
+    pluralDeterminerHours = " hours ";
+  } else if (pluralDeterminerHours == 0) {
+    pluralDeterminerHours = " hours ";
+  }
+  if (minuteManipulator > 1) {
+    pluralDeterminerMinutes = " minutes ";
+  } else if (minuteManipulator == 0) {
+    pluralDeterminerMinutes = " minutes";
+  }
+
+  console.log(
+    hourManipulator +
+      pluralDeterminerHours +
+      ", " +
+      minuteManipulator +
+      pluralDeterminerMinutes +
+      "."
+  );
 }
-if (minuteManipulator > 1) {
-  pluralDeterminerMinutes = " minutes ";
-} else if (minuteManipulator == 0) {
-  pluralDeterminerMinutes = " minutes";
-}
-
-console.log(
-  hourManipulator +
-    pluralDeterminerHours +
-    ", " +
-    minuteManipulator +
-    pluralDeterminerMinutes +
-    "."
-);
+realNumberToTime(71);
