8447624a2187c854b27c3664e4cce6b16982cbcd
Jacky Baloyi <87927960+JayBal@users.noreply.github.com>
2021-08-20 11:49:29 +0000

    Update task0-8.js

--- task0-8.js
+++ task0-8.js
@@ -1,10 +1,9 @@
 function realNumberToTime(number) {
   var initialHour;
 
-  //var number = 71;
   initialHour = number / 60;
 
-  hourManipulator = parseFloat(initialHour).toFixed(0); // I came up with this just so I can eleminate decimals
+  hourManipulator = parseFloat(initialHour).toFixed(0); // I came up with this idea just so I can eleminate decimals
   //hourManipulator = initialHour; : This uncommented will produce 1.18 with 71 as the given number.
   minuteManipulator = number % 60;
   final = parseFloat(hourManipulator) + parseFloat(minuteManipulator);
@@ -12,10 +11,15 @@ function realNumberToTime(number) {
   var pluralDeterminerHours = " hour";
   var pluralDeterminerMinutes = " minute ";
 
+  if (number < 60) {
+    hourManipulator = 0;
+    pluralDeterminerHours = " hours";
+  }
+
   if (hourManipulator > 1) {
     pluralDeterminerHours = " hours ";
-  } else if (pluralDeterminerHours == 0) {
-    pluralDeterminerHours = " hours ";
+  } else if (pluralDeterminerHours == 0 && pluralDeterminerHours == 1) {
+    pluralDeterminerHours = " hour ";
   }
   if (minuteManipulator > 1) {
     pluralDeterminerMinutes = " minutes ";
@@ -32,4 +36,4 @@ function realNumberToTime(number) {
       "."
   );
 }
-realNumberToTime(71);
+realNumberToTime(55);
