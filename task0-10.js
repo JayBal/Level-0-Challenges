87819276d3c9a8f7fd4e57c7f652b5c33a20aa52
Jacky Baloyi <87927960+JayBal@users.noreply.github.com>
2021-09-17 13:06:59 +0000

    Update task0-10.js
    Eslint code style.

--- task0-10.js
+++ task0-10.js
@@ -1,19 +1,22 @@
 function commonCharacterFinder(inputOne, inputTwo) {
-  var charFoundHolder = " ";
+  let charFoundHolder = '';
   if (inputOne.length > inputTwo.length) {
-    for (let i = 0; i < inputOne.length; i++) {
+    for (let i = 0; i < inputOne.length; i += 1) {
       if (inputOne.includes(inputTwo[i])) {
-        charFoundHolder += inputTwo[i] + ",";
+        charFoundHolder += inputTwo[i];
       }
     }
   } else {
-    for (let i = 0; i < inputTwo.length; i++) {
+    for (let i = 0; i < inputTwo.length; i += 1) {
       if (inputOne.includes(inputTwo[i])) {
-        charFoundHolder += inputTwo[i] + " ";
+        charFoundHolder += `${inputTwo[i]}`;
       }
     }
   }
-  console.log("Common letters:" + charFoundHolder);
+  const outputSet = new Set(charFoundHolder);
+  const finalResults = [...outputSet].join(', ');
+
+  console.log(`Common letters: ${finalResults}.`);
 }
 
-commonCharacterFinder("computers", "house");
+commonCharacterFinder('computers', 'house');
