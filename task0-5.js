891e91e1ac3c1d798c49a204f660d1c819ae2d27
Jacky Baloyi <87927960+JayBal@users.noreply.github.com>
2021-09-17 13:02:01 +0000

    Update task0-5.js
    Eslint edited code style.

--- task0-5.js
+++ task0-5.js
@@ -1,12 +1,8 @@
-function areaOfATriangle(a, b, c){
+function areaOfATriangle(a, b, c) {
+  const semiPerimeter = (a + b + c) / 2;
 
-  var semiPerimeter =  (a + b + c) / 2;
-
-  var finalArea = Math.sqrt(semiPerimeter * ((semiPerimeter - a) * (semiPerimeter - b) * (semiPerimeter - c)));
-
+  const finalArea = Math.sqrt(semiPerimeter * ((semiPerimeter - a) * (semiPerimeter - b) * (semiPerimeter - c)));
   return finalArea;
-  
-  }
-
-areaOfATriangle(6,7,8);
+}
+areaOfATriangle(6, 7, 8);
 
