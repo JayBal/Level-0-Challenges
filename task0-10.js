d22d44814d7abb20a1fed8fb9dc9651566c5b7a8
Jacky Baloyi <87927960+JayBal@users.noreply.github.com>
2021-10-06 14:19:54 +0000

    Update task0-10.js
    Removed the second loop

--- task0-10.js
+++ task0-10.js
@@ -1,22 +1,17 @@
 function commonCharacterFinder(inputOne, inputTwo) {
   let charFoundHolder = '';
-  if (inputOne.length > inputTwo.length) {
-    for (let i = 0; i < inputOne.length; i += 1) {
-      if (inputOne.includes(inputTwo[i])) {
-        charFoundHolder += inputTwo[i];
-      }
-    }
-  } else {
-    for (let i = 0; i < inputTwo.length; i += 1) {
-      if (inputOne.includes(inputTwo[i])) {
-        charFoundHolder += `${inputTwo[i]}`;
-      }
+  const longInput = inputOne.length;
+  const shortInput = inputTwo.length;
+  const lengthDeterminer = Math.max(longInput, shortInput);
+
+  for (let i = 0; i < lengthDeterminer; i += 1) {
+    if (inputOne.includes(inputTwo[i])) {
+      charFoundHolder += inputTwo[i];
     }
   }
   const outputSet = new Set(charFoundHolder);
   const finalResults = [...outputSet].join(', ');
-
-  console.log(`Common letters: ${finalResults}.`);
+  console.log(`Common letters: ${finalResults}`);
 }
 
 commonCharacterFinder('computers', 'house');
